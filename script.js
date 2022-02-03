9422b1fe681068b645ca7831784c42823bc8b849
Abdel Bailoul <ab.bailoul@gmail.com>
2022-02-03 15:21:52 +0100

    Second commit /  udapte images names - clean scss - add alt on imgs of html

--- script.js
+++
@@ -1,9 +0,0 @@
-let sum = 0;
-let arr = [2, 6, 18, 90];
-let arr2 = [];
-arr.forEach((element, index) => {
-  sum += index % 2 && element;
-  index % 2 && arr2.push(element);
-});
-console.log(sum);
-console.log(arr2);
